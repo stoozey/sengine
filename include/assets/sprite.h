ad6c95755313ef63840c8d3bc2256995df90f03e
stoozey_ <stoozey123@gmail.com>
2023-02-14 06:50:14 +0000

    assets/sprite finished

--- include/assets/sprite.h
+++ include/assets/sprite.h
@@ -2,15 +2,28 @@
 #define SENGINE_SPRITE_H
 
 #include <SDL.h>
+#include <SDL_image.h>
 
 #include "engine.h"
 #include "assets/asset.h"
+#include "datatypes/vector2.h"
 
 class Sprite : public Asset {
 public:
     Sprite(const std::string& filePath, SDL_Renderer *sdlRenderer);
+    ~Sprite();
+
+    void Render(const Vector2 &position);
+
+    SDL_FRect GetFRect(const Vector2 &position);
+    int GetWidth();
+    int GetHeight();
 private:
     SDL_Renderer *renderer;
+    SDL_Texture *texture;
+
+    int width;
+    int height;
 };
 
 #endif //SENGINE_SPRITE_H
--- src/assets/sprite.cpp
+++ src/assets/sprite.cpp
@@ -3,4 +3,24 @@
 Sprite::Sprite(const std::string& filePath, SDL_Renderer *sdlRenderer) : Asset(filePath) {
     renderer = ((sdlRenderer == nullptr) ? g_Engine->GetRenderer() : sdlRenderer);
 
+    SDL_Surface *surface = IMG_Load(filePath.c_str());
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+
+    SDL_QueryTexture(texture, NULL, NULL, &width, &height);
+}
+
+Sprite::~Sprite() {
+    if (texture) {
+        SDL_DestroyTexture(texture);
+    }
+}
+
+SDL_FRect Sprite::GetFRect(const Vector2 &position) {
+    return { position.x, position.y, (float) width, (float) height };
+}
+
+void Sprite::Render(const Vector2 &position) {
+    SDL_FRect rect = GetFRect(position);
+    SDL_RenderCopyF(renderer, texture, nullptr, &rect);
 }
