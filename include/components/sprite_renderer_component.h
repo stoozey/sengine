11c10227c308caa350b09a9b46f3d0fab92e7f91
stoozey_ <stoozey123@gmail.com>
2023-02-14 07:13:56 +0000

    Update sprite_renderer_component.h

---
+++ include/components/sprite_renderer_component.h
@@ -0,0 +1,11 @@
+#ifndef SENGINE_SPRITE_RENDERER_COMPONENT_H
+#define SENGINE_SPRITE_RENDERER_COMPONENT_H
+
+#include "components/component.h"
+
+class SpriteRendererComponent : Component {
+private:
+
+};
+
+#endif //SENGINE_SPRITE_RENDERER_COMPONENT_H
